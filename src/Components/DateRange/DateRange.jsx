494dd69545beaae962664ef6dcd6ce5d2ab6ead2
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-03-29 19:27:47 +0400

    added dateRange

---
+++ src/Components/DateRange/DateRange.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+// Components
+import { DateRangePicker } from "react-date-range";
+import { addDays } from "date-fns";
+
+// Styles
+import Style from "./DateRange.module.css";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+const DateRange = ({ ...props }) => {
+  // Variables
+  const [show, setShow] = useState(false);
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+
+  function formatDate(dateString) {
+    const dateObj = new Date(dateString);
+    const day = dateObj.getDate();
+    const month = dateObj.toLocaleString("default", { month: "short" });
+    const year = dateObj.getFullYear();
+
+    return `${day < 10 ? "0" : ""}${day} ${month} ${year}`;
+  }
+
+  return (
+    <div className={Style.rangeWrapper} onClick={() => setShow(true)}>
+      <p>{props.label}</p>
+      <div
+        style={{
+          display: show ? "block" : "none",
+        }}
+        className={Style.range}
+      >
+        <DateRangePicker
+          onChange={(item) => {
+            setState([item.selection]);
+            console.log(item.selection.endDate);
+            if (item.selection) {
+              setShow(false);
+            }
+          }}
+          showSelectionPreview={true}
+          moveRangeOnFirstSelection={false}
+          months={1}
+          ranges={state}
+          direction="horizontal"
+        />
+      </div>
+      <p>{`${
+        state[0]["startDate"] ? formatDate(state[0]["startDate"]) : "DD/MM/YYY"
+      } - ${
+        state[0]["endDate"] ? formatDate(state[0]["endDate"]) : "DD/MM/YYY"
+      }`}</p>
+    </div>
+  );
+};
+
+export default DateRange;
--- src/Views/HomeView/Components/HeroSection/HeroSection.jsx
+++ src/Views/HomeView/Components/HeroSection/HeroSection.jsx
@@ -2,24 +2,15 @@ import React, { useState } from "react";
 
 // Components
 import SimpleSelect from "../../../../Components/SimpleSelect/SimpleSelect";
-import { DateRangePicker } from "react-date-range";
-import { addDays } from "date-fns";
+import DateRange from "../../../../Components/DateRange/DateRange";
 
 // Styles
 import Style from "./HeroSection.module.css";
-import "react-date-range/dist/styles.css"; // main css file
-import "react-date-range/dist/theme/default.css"; // theme css file
 
 const HeroSection = () => {
   const [filter, setFilter] = useState("");
   const f = ["Dubai", "Sharjah", "Abu Dhabi"];
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: "selection",
-    },
-  ]);
+
   return (
     <div className={Style.hero}>
       <h2>Helping Others</h2>
@@ -34,18 +25,7 @@ const HeroSection = () => {
           setFilter(value);
         }}
       />
-      <DateRangePicker
-        onChange={(item) => setState([item.selection])}
-        showSelectionPreview={true}
-        moveRangeOnFirstSelection={false}
-        months={1}
-        ranges={state}
-        direction="horizontal"
-        color="red"
-        className={Style.range}
-        staticRanges={[]}
-        inputRanges={[]}
-      />
+      <DateRange label={"Depart - Return"} />
     </div>
   );
 };
