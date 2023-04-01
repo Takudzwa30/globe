cdcd8c5e6ca85f76151a3afd55d00cb5661b7aae
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-04-01 10:33:46 +0400

    iniit flights annd hotels

---
+++ src/Views/HomeView/Components/FlightandHotels/FlightandHotels.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Styles
+import Style from "./FlightandHotels.module.css";
+
+const FlightandHotels = () => {
+  return <div>FlightandHotels</div>;
+};
+
+export default FlightandHotels;
