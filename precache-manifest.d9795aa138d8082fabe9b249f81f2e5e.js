16ca0d2ae9b025dd56e8fb1395fb5c2b9f7425cd
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-04-23 01:19:10 +0400

    Updates

---
+++ precache-manifest.d9795aa138d8082fabe9b249f81f2e5e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe05ce5ebea81537331d",
+    "url": "/globe/static/css/main.8896366d.chunk.css"
+  },
+  {
+    "revision": "fe05ce5ebea81537331d",
+    "url": "/globe/static/js/main.fe05ce5e.chunk.js"
+  },
+  {
+    "revision": "8ad47d1adc123bb28c75",
+    "url": "/globe/static/js/1.8ad47d1a.chunk.js"
+  },
+  {
+    "revision": "5cffa0cb2b57b7ea820e",
+    "url": "/globe/static/css/2.d5914fa6.chunk.css"
+  },
+  {
+    "revision": "5cffa0cb2b57b7ea820e",
+    "url": "/globe/static/js/2.5cffa0cb.chunk.js"
+  },
+  {
+    "revision": "660a6db9b013ac34dc1a",
+    "url": "/globe/static/js/runtime~main.660a6db9.js"
+  },
+  {
+    "revision": "5414ee0fae6066b34851562d98126623",
+    "url": "/globe/static/media/logoLight.5414ee0f.svg"
+  },
+  {
+    "revision": "b90494787a94033ef7039102e2122a87",
+    "url": "/globe/static/media/flightsBG.b9049478.png"
+  },
+  {
+    "revision": "1bdc2f3065dad0c2ada4276d58c44234",
+    "url": "/globe/static/media/hotelsBG.1bdc2f30.png"
+  },
+  {
+    "revision": "1505e237d212631ce171999d6f403459",
+    "url": "/globe/static/media/iceViews.1505e237.png"
+  },
+  {
+    "revision": "2b6b62806d86d64e53d366361e085cd2",
+    "url": "/globe/static/media/logoDark.2b6b6280.svg"
+  },
+  {
+    "revision": "e2711ebf6d2ab39b1cffd1eb09459234",
+    "url": "/globe/static/media/homeHero.e2711ebf.png"
+  },
+  {
+    "revision": "4ba0f66c52145de0284cd90adadc2b5f",
+    "url": "/globe/static/media/destinationTurkey.4ba0f66c.png"
+  },
+  {
+    "revision": "93a383ff51e898cdc17e7eae371e8056",
+    "url": "/globe/static/media/sydneyImage.93a383ff.png"
+  },
+  {
+    "revision": "527507e051ab899fea18ecc560f4495d",
+    "url": "/globe/static/media/bakuImage.527507e0.png"
+  },
+  {
+    "revision": "270cd1364d06daa13cd53e0b9cd4bb1d",
+    "url": "/globe/static/media/maleImage.270cd136.png"
+  },
+  {
+    "revision": "42af1f136bff52716fe42579d877f557",
+    "url": "/globe/static/media/parisImage.42af1f13.png"
+  },
+  {
+    "revision": "b941b2f04117dc5b75b43442f7949435",
+    "url": "/globe/static/media/NYImage.b941b2f0.png"
+  },
+  {
+    "revision": "40a3aa077ec7a28845586e8a7a8f11ad",
+    "url": "/globe/static/media/londonImage.40a3aa07.png"
+  },
+  {
+    "revision": "f95c89f20ed065cfdfea61a27a07931c",
+    "url": "/globe/static/media/tokyoImage.f95c89f2.png"
+  },
+  {
+    "revision": "73f5891e7a2cc219f25760d6600d9788",
+    "url": "/globe/static/media/dubaiImage.73f5891e.png"
+  },
+  {
+    "revision": "dd9f31aca110d205e7a3b9987c32bad5",
+    "url": "/globe/static/media/northernLights.dd9f31ac.png"
+  },
+  {
+    "revision": "4bb4b8702217be5dfbee418d3e4c1d3b",
+    "url": "/globe/static/media/cave.4bb4b870.png"
+  },
+  {
+    "revision": "1adce5529e1ad46bd4bd3d9156e3906e",
+    "url": "/globe/index.html"
+  }
+];
