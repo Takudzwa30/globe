7852d28453728749fd1d88e17c06625d8a8d81cb
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-04-23 00:37:41 +0400

    added data

--- src/Views/HomeView/Components/Plan/Plan.jsx
+++ src/Views/HomeView/Components/Plan/Plan.jsx
@@ -3,8 +3,19 @@ import React from "react";
 // Libraries
 import { Container } from "@hybris-software/ui-kit";
 
+// Components
+import CustomButton from "../../../../Components/CustomButton/CustomButton";
+
 // Images
 import turkey from "../../../../Assets/images/destinationTurkey.png";
+import sydney from "../../../../Assets/images/sydneyImage.png";
+import baku from "../../../../Assets/images/bakuImage.png";
+import male from "../../../../Assets/images/maleImage.png";
+import paris from "../../../../Assets/images/parisImage.png";
+import NY from "../../../../Assets/images/NYImage.png";
+import london from "../../../../Assets/images/londonImage.png";
+import tokyo from "../../../../Assets/images/tokyoImage.png";
+import dubai from "../../../../Assets/images/dubaiImage.png";
 
 // Style
 import Style from "./Plan.module.css";
@@ -16,36 +27,36 @@ const places = [
     destination: "Istanbul, Turkey",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: sydney,
+    destination: "Sydney, Australia",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: baku,
+    destination: "Baku, Azerbaijan",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: male,
+    destination: "Malé, Maldives",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: paris,
+    destination: "Paris, France",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: NY,
+    destination: "New York, US",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: london,
+    destination: "London, UK",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: tokyo,
+    destination: "Tokyo, Japan",
   },
   {
-    image: turkey,
-    destination: "Istanbul, Turkey",
+    image: dubai,
+    destination: "Dubai, UAE",
   },
 ];
 
@@ -57,7 +68,7 @@ const Plan = () => {
           <h3>Plan your perfect trip</h3>
           <h6>Search Flights & Places Hire to our most popular destinations</h6>
         </div>
-        <div className={Style.seeMore}>See more places</div>
+        <CustomButton>See more places</CustomButton>
       </div>
       <div className={Style.cards}>
         {places.map((place, index) => {
