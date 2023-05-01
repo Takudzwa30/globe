73471a2c452de4d3a01147e79ff762b2eec19586
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-05-01 23:51:19 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/globe/precache-manifest.0cf83c573a410deb6fc8b162e2dd1ad8.js"
+  "/globe/precache-manifest.ce6fcca3879045fc1ac6b6d059f8cfe7.js"
 );
 
 workbox.clientsClaim();
