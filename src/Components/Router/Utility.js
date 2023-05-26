50c578e755a6c38ee625daa92fe29cefeac9e202
Takudzwa Mushai <takudzwa.mushai@jcmanagement.com>
2023-05-26 09:43:54 +0400

    fixed page title

--- src/Components/Router/Utility.js
+++ src/Components/Router/Utility.js
@@ -2,16 +2,20 @@
 import { routes } from "../../Routes/routes";
 
 const getPageTitle = (pathName) => {
-  for (let x in Object.entries(routes)) {
-    const [, layoutData] = Object.entries(routes)[x];
-    for (let y in Object.entries(layoutData.routes)) {
-      const [, routeData] = Object.entries(layoutData.routes)[y];
-      if (layoutData.path.concat(routeData.path) === pathName) {
-        return `Globe | ${routeData.title}`;
+  if (pathName === "/globe") {
+    return "Globe | Home Page";
+  } else {
+    for (let x in Object.entries(routes)) {
+      const [, layoutData] = Object.entries(routes)[x];
+      for (let y in Object.entries(layoutData.routes)) {
+        const [, routeData] = Object.entries(layoutData.routes)[y];
+        if (layoutData.path.concat(routeData.path) === pathName) {
+          return `Globe | ${routeData.title}`;
+        }
       }
     }
+    return "Globe | 404 Not Found";
   }
-  return "Globe | 404 Not Found";
 };
 
 export default getPageTitle;
