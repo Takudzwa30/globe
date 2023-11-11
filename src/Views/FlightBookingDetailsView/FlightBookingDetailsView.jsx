cf4aea9b129879f419d63fbeba035fee6834e781
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-11-11 20:52:33 +0400

    added flight booking

--- src/Views/FlightBookingDetailsView/FlightBookingDetailsView.jsx
+++ src/Views/FlightBookingDetailsView/FlightBookingDetailsView.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 
-// Styles
-import chevronForward from "../../Assets/images/chevronForward.png";
-
-import Style from "./FlightBookingDetailsView.module.css";
+// Libraries
 import { Container } from "@hybris-software/ui-kit";
+
+// Components
 import FlightCard from "../../Components/ui/FlightCard/FlightCard";
 import PriceBreakdown from "../../Components/ui/PriceBreakdown/PriceBreakdown";
+import Payment from "../../Components/ui/payment/Payment";
+
+// Icons
+import chevronForward from "../../Assets/images/chevronForward.png";
+
+// Styles
+import Style from "./FlightBookingDetailsView.module.css";
 
 const FlightBookingDetailsView = () => {
-   return (
-      <Container>
-         <div className={Style.breadCrumb}>
-            <p>Turkey</p>
-            <img src={chevronForward} alt="" />
-            <p>Istanbul</p>
-            <img src={chevronForward} alt="" />
-            <p>CVK Park Bosphorus Hotel Istanbul</p>
-         </div>
-         <div className={Style.detailsGrid}>
-            <FlightCard version={true} />
-            <PriceBreakdown />
-         </div>
-      </Container>
-   );
+  return (
+    <Container>
+      <div className={Style.breadCrumb}>
+        <p>Turkey</p>
+        <img src={chevronForward} alt="" />
+        <p>Istanbul</p>
+        <img src={chevronForward} alt="" />
+        <p>CVK Park Bosphorus Hotel Istanbul</p>
+      </div>
+      <div className={Style.detailsGrid}>
+        <FlightCard version={true} />
+        <PriceBreakdown version="stay" />
+        <Payment />
+        <Payment />
+      </div>
+    </Container>
+  );
 };
 
 export default FlightBookingDetailsView;
