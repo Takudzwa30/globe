afee9c63e69c613d9c58ad89fae8ef976d76a194
takudzwa-mushai <takudzwa.mushai@vaimanagement.co>
2023-11-16 11:00:53 +0400

    fix

--- src/Views/StaysListingView/StaysListingView.jsx
+++ src/Views/StaysListingView/StaysListingView.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Container } from "@hybris-software/ui-kit";
+
 const StaysListingView = () => {
-   return <div>StaysListingView</div>;
+   return (
+      <Container>
+         <h1>StaysListingView</h1>
+      </Container>
+   );
 };
 
 export default StaysListingView;
