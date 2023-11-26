01eef032fb4f54f67f8442a56bc7780ddacde09b
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-11-26 18:55:48 +0400

    fixed state changes

--- src/Components/Like/Like.jsx
+++ src/Components/Like/Like.jsx
@@ -8,9 +8,9 @@ import Style from "./Like.module.css";
 
 const Like = ({ like, setLike }) => {
   return (
-    <div className={Style.favorite} onClick={() => setLike(!like)}>
+    <div className={Style.favorite} onClick={() => setLike((prev) => !prev)}>
       {" "}
-      {like ? (
+      {!like ? (
         <AiOutlineHeart className={Style.like} />
       ) : (
         <AiFillHeart className={Style.disLike} />
