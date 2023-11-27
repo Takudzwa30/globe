5f2310b7b916bafa3d09549504e8f64767468d0b
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-11-27 12:19:33 +0400

    init auth Heading

---
+++ src/Components/ui/authHeading/AuthHeading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Style
+import Style from "./AuthHeading.module.css";
+
+const AuthHeading = () => {
+  return <div>AuthHeading</div>;
+};
+
+export default AuthHeading;
